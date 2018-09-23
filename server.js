e9e838b55b5cce736233cd1be19749d7463bca2d
ValentinTT <valentintapiatorti@gmail.com>
2018-09-23 16:40:59 -0300

    Connection to the db and basic routing to the oauth
    The connection to the db was made from the server. At same time there
    were added the necessary routes for the oauth authentication

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
-console.log("Hola mundo");
+require('dotenv').config();
+require('./db/connection').initDB();
 
-require("dotenv").config();
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const passport = require('passport');
+require('./passport-setup');
+const cookieSession = require('cookie-session');
+const authRoutes = require('./routes/auth-routes').Router;
+const profileRoutes = require('./routes/profile-router').Router;
+const port = process.env.PORT || 3000;
 
 const app = express();
-app.use(bodyParser.json());
+app.set('view engine', 'pug');
+app.use(express.static(__dirname + '/public'));
 
+//Cookie and sessions with passport
+app.use(
+  cookieSession({
+    maxAge: 1000 * 60 * 60,
+    name: 'session',
+    keys: [process.env.COOKIE_SESSION_KEYS]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get("/", (req, res) => {
-  res.send("Bienvenido a beeAnalizer.com");
+app.use('/auth', authRoutes);
+app.use('/profile', profileRoutes);
+
+app.get('/', (req, res, next) => {
+  if (!req.user) res.redirect('auth/login');
+  else res.render('/profile');
 });
 
-app.listen(4000, () => {
-  console.log("App listening at port", 4000);
-});
+app.listen(port, () => console.log('App listening at port', port));
